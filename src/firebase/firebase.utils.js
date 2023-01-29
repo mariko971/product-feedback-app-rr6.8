6a36f28e04760b1b39db69a8a8224c189d2e5f65
Mark Njuru <66862952+mariko971@users.noreply.github.com>
2023-01-29 17:17:18 +0400

    Stored API key in an environment variable.

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,39 +1,41 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/firestore';
-import { collection, getDocs } from "firebase/firestore"; 
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import { collection, getDocs } from "firebase/firestore";
 
 const config = {
-  apiKey: "AIzaSyCpKr0RcbpzW0oruiB7GFnirNtlyHaRQ-0",
+  apiKey: process.env.API_KEY_FIREBASE,
   authDomain: "app-feedback-1f16f.firebaseapp.com",
   projectId: "app-feedback-1f16f",
   storageBucket: "app-feedback-1f16f.appspot.com",
   messagingSenderId: "694762859569",
-  appId: "1:694762859569:web:3f69bb8a264a3c68d93910"
-  };
-
-
-  export const addCollectionAndDocuments = async (collectionKey, objectsToAdd)=>{
-    const collectionRef= firestore.collection(collectionKey);
-    console.log(collectionRef)
-    const batch = firestore.batch();
-    objectsToAdd.forEach(obj=>{
-      const newDocRef = collectionRef.doc(`${obj.id}`);
-      batch.set(newDocRef,obj)
-    });
-    return await batch.commit()
+  appId: "1:694762859569:web:3f69bb8a264a3c68d93910",
 };
 
-export const getData = async ()=>{
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey);
+  console.log(collectionRef);
+  const batch = firestore.batch();
+  objectsToAdd.forEach((obj) => {
+    const newDocRef = collectionRef.doc(`${obj.id}`);
+    batch.set(newDocRef, obj);
+  });
+  return await batch.commit();
+};
+
+export const getData = async () => {
   const querySnapshot = await getDocs(collection(firestore, "productRequests"));
   const appData = [];
-  querySnapshot.forEach((doc) => {    
-    appData.push(doc.data());  
-});
-console.log(appData);
-}
+  querySnapshot.forEach((doc) => {
+    appData.push(doc.data());
+  });
+  return appData;
+};
 
-  firebase.initializeApp(config);
+firebase.initializeApp(config);
 
-  export const firestore = firebase.firestore();
+export const firestore = firebase.firestore();
 
-  export default firebase;
+export default firebase;
